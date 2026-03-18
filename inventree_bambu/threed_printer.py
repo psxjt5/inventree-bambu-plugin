8bc08571d9fab1e10a3776a7cfc60e142a6e80cf
James Todd <James.Todd@nottingham.ac.uk>
2026-03-18 17:07:58 +0000

    Added django imports.

--- inventree_bambu/threed_printer.py
+++ inventree_bambu/threed_printer.py
@@ -1,5 +1,13 @@
 """3D Printer machine type."""
 
+from typing import cast
+
+from django.contrib.auth.models import AnonymousUser
+from django.db import models
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.utils.translation import gettext_lazy as _
+
 from generic.states import ColorEnum
 from machine.machine_type import BaseDriver, BaseMachineType, MachineStatus
 from stock.models import StockLocation
