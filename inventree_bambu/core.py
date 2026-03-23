456631d4af7ff93e2e09cd4514c89c599ec1718e
James Todd <James.Todd@nottingham.ac.uk>
2026-03-23 22:46:29 +0000

    Status debug.

--- inventree_bambu/core.py
+++ inventree_bambu/core.py
@@ -168,9 +168,9 @@ class BambuLabPrinterDriver(ThreeDPrinterBaseDriver):
         return mapping.get(state, ThreeDPrinterStatus.UNKNOWN)
 
     def get_status(self, machine):
+        print(f"[BambuLabPrinterDriver] Getting status for {machine.name}")
         return ThreeDPrinterStatus.UNKNOWN
 
-        print(f"[BambuLabPrinterDriver] Getting status for {machine.name}")
         serial = machine.get_setting("SERIAL", "D")
         if not serial:
             return ThreeDPrinterStatus.UNKNOWN
