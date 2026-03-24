f174805dea1ab4b8799ba768148ebe36d6830396
James Todd <James.Todd@nottingham.ac.uk>
2026-03-24 11:52:46 +0000

    Status removed value.

--- inventree_bambu/core.py
+++ inventree_bambu/core.py
@@ -96,7 +96,7 @@ class BambuLabPrinterDriver(ThreeDPrinterBaseDriver):
 
     def initialise(self, machine):
         print(f"[BambuLabPrinterDriver] Initialising Machine {machine.name}")
-        machine.set_status(machine.MACHINE_STATUS.UNKNOWN.value)
+        machine.set_status(machine.MACHINE_STATUS.UNKNOWN)
         print("[BambuLabPrinterDriver] Machine status class:", machine.MACHINE_STATUS)
         print("[BambuLabPrinterDriver] Unknown value:", machine.MACHINE_STATUS.UNKNOWN)
         print("[BambuLabPrinterDriver] Choices:", getattr(machine.MACHINE_STATUS, 'choices', None))
