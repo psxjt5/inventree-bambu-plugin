944bd4a5af15b5a27c2cf8deaad229355077da48
James Todd <James.Todd@nottingham.ac.uk>
2026-03-24 11:55:57 +0000

    Add status class to init.

--- inventree_bambu/__init__.py
+++ inventree_bambu/__init__.py
@@ -2,4 +2,4 @@
 
 PLUGIN_VERSION = "0.1.0"
 
-from .threed_printer import ThreeDPrinterBaseDriver, ThreeDPrinterMachine
+from .threed_printer import ThreeDPrinterBaseDriver, ThreeDPrinterMachine, ThreeDPrinterStatus
