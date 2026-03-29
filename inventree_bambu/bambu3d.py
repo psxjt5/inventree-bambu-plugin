98e1f01bb3daa5668ffdf2fecdd41c4a3c3929ba
James Todd <James.Todd@nottingham.ac.uk>
2026-03-29 20:51:26 +0100

    Added setting presence check.

--- inventree_bambu/bambu3d.py
+++ inventree_bambu/bambu3d.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.utils.translation import gettext_lazy as _
 
 from inventree_3d.threed import ThreeDPrinterBaseDriver, ThreeDPrinterMachine
+from .bambumqttmanager import BambuMQTTManager
 
 class BambuLab3DPrinterDriver(ThreeDPrinterBaseDriver):
     """BambuLab 3D Printing machine driver."""
@@ -39,4 +40,30 @@ class BambuLab3DPrinterDriver(ThreeDPrinterBaseDriver):
 
     def init_machine(self, machine):
         """Machine initialise hook"""
-        machine.set_status(ThreeDPrinterMachine.MACHINE_STATUS.PRINTING)
+        print(f"[BambuLab3DPrinterDriver] Initialising Machine {machine.name}")
+
+        # Check machine settings have been filled
+        if not self.validate_required_settings(machine):
+            print(f"[BambuLab3DPrinterDriver] Machine misconfigured {machine.name}")
+            machine.set_status(ThreeDPrinterMachine.MACHINE_STATUS.MISCONFIGURED)
+            return
+        
+    def validate_required_settings(self, machine) -> bool:
+        """
+        Ensure that all required machine settings are filled.
+
+        Returns True if valid, False if any are missing.
+        """
+        required_fields = ["IP_ADDRESS", "ACCESS_TOKEN", "SERIAL"]
+
+        missing = []
+        for field in required_fields:
+            value = machine.get_setting(field, "D")
+            if not value:
+                missing.append(field)
+
+        if missing:
+            print(f"[BambuLabPrinterDriver] Machine '{machine.name}' missing required settings: {missing}")
+            return False
+
+        return True
