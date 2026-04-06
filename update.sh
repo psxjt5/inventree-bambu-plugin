c1bc9f052b307981f0a59ee86e5f95074f43ccbb
James Todd <James.Todd@nottingham.ac.uk>
2026-04-06 21:04:56 +0100

    Added update script.

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e  # Exit on error
+
+CONTAINER="inventree-server"
+
+echo "Updating plugins inside container: $CONTAINER"
+
+# Run everything inside the container
+docker exec -i $CONTAINER bash << 'EOF'
+
+set -e
+
+PLUGIN1_DIR="/home/inventree/data/inventree-3d-printing"
+PLUGIN2_DIR="/home/inventree/data/inventree-bambu-plugin"
+STATIC_DIR="/home/inventree/data/static/plugins/inventree_bambu"
+
+echo "Pulling latest code..."
+
+cd $PLUGIN1_DIR
+git pull
+
+cd $PLUGIN2_DIR
+git pull
+
+echo "Copying frontend assets..."
+
+rm -rf $STATIC_DIR/*
+cp -r $PLUGIN1_DIR/inventree_bambu/static/* $STATIC_DIR/
+
+echo "Plugin update complete"
+
+EOF
+
+echo "Restarting container..."
+docker restart $CONTAINER
+
+echo "Done!"
