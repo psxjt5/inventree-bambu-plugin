15e8d33e9836359e14bef9e8280469af96fb6ddf
James Todd <James.Todd@nottingham.ac.uk>
2026-04-09 23:41:23 +0100

    Added comments.

--- inventree_bambu/bambuapi.py
+++ inventree_bambu/bambuapi.py
@@ -15,10 +15,12 @@ class BambuAPI:
     def get_printer_data(request, machine_serial):
         """Return data for a specific printer"""
 
+        print("[BambuAPI] Get Printer Data")
+
         data = {
             "serial": machine_serial,
             "status": BambuData.getStatus(machine_serial),
-            "model": BambuData.getStatus(machine_serial),
+            "model": BambuData.getModel(machine_serial),
             "ams_count": BambuData.getAMSUnitCount(machine_serial)
         }
 
