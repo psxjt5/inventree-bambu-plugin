a43d4fca605b27e40fe2eba424d273ef2382de18
James Todd <James.Todd@nottingham.ac.uk>
2026-05-04 16:25:39 +0100

    Updated import path.

--- inventree_bambu/bambuapi.py
+++ inventree_bambu/bambuapi.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from InvenTree.machine.models import Machine
+from inventree.machine.models import Machine
 
 from .bambudata import BambuData
 
