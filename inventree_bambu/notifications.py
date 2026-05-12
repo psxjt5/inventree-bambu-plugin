9d169e66c86b61abafd456b854e7714c6076b234
James Todd <James.Todd@nottingham.ac.uk>
2026-05-12 20:31:04 +0100

    Minimal notification.

--- inventree_bambu/notifications.py
+++ inventree_bambu/notifications.py
@@ -6,33 +6,22 @@ from common.notifications import trigger_notification
 from django.contrib.auth import get_user_model
 from machine.models import MachineConfig
 
-User = get_user_model()
-
 class Notifications:
 
     def test_notification(machine):
         print("[BambuLab3DPrintingNotifications] Sending Test Notification.")
 
-        users = User.objects.filter(is_active=True)
+        User = get_user_model()
 
-        # trigger_notification(
-        #     obj=machine,
-        #     category="machine.bambu",
-        #     targets=users,
-        #     context={
-        #         "name": "Test Notification",
-        #         "message": f"Test Notification Text",
-        #     },
-        # )
-
-        machine = MachineConfig.objects.get(pk=machine.pk)
+        user = User.objects.filter(is_superuser=True).first()
 
         trigger_notification(
-            obj=machine,
+            obj=None,
             category="system",
-            targets=User.objects.filter(is_superuser=True),
+            targets=[user],
             context={
-                "name": "HELLO",
-                "message": "If you see this, pipeline works",
+                "name": "Test",
+                "message": "Hello world",
             },
+            check_recent=False,
         )
