308d0f1f6bb35065e6c90dabaa2314994f30cae1
James Todd <James.Todd@nottingham.ac.uk>
2026-06-08 22:14:04 +0100

    Added UID Tracing code.

--- inventree_bambu/notifications.py
+++ inventree_bambu/notifications.py
@@ -11,11 +11,17 @@ class Notifications:
     def test_notification(machine):
         print("[BambuLab3DPrintingNotifications] Sending Test Notification.")
 
+        print(machine.pk, type(machine.pk))
+        print(machine.uid, type(machine.uid))
+        print(machine.id, type(machine.id))
+
         User = get_user_model()
 
         user = User.objects.filter(is_superuser=True)
 
         machine = MachineConfig.objects.get(pk=machine.pk)
+
+        print(f"Machine UID: {getattr(machine, 'uid', None)}")
 
         trigger_notification(
             obj=machine,
